b8d46aee5df39e80ea8d4460031d0b87ea7b5e09
Rayelle <equation0703@naver.com>
2024-02-01 19:41:50 +0900

    IDamageable Interface 스크립트
    총기 타격성공여부 인터페이스 작성

--- Assets/Scripts/DamageMessage.cs
+++ Assets/Scripts/DamageMessage.cs
@@ -1,10 +1,19 @@
 ﻿using UnityEngine;
 
+//클래스로 만들면 ref타입이라 메세지를 전달받은 측에서 데미지 메세지를 임의로 수정하면 
+//같은 메세지를 전달받은 다른 곳에서도 해당 변경사항이 반영된다
+
+//구조체같은 value타입은 데미지 메세지를 전달받은 측에서 필요에따라 메세지 내용을 
+//마음대로 수정해도 다른 곳에 영향을 미치지않는다
 public struct DamageMessage
 {
+    //공격을 가한 오브젝트
     public GameObject damager;
+    //공격 데미지양
     public float amount;
 
+    //공격이 가해진 위치
     public Vector3 hitPoint;
+    //공격을 맞은 표면이 바라보는 방향 or 공격이 가해진 방향의 반대 방향
     public Vector3 hitNormal;
 }
--- Assets/Scripts/IDamageable.cs
+++ Assets/Scripts/IDamageable.cs
@@ -1,4 +1,11 @@
 ﻿public interface IDamageable
 {
+
+    //공격받을수 있는 모든 오브젝트를 이 인터페이스를 사용해서 구현하도록 강제한다
+    //인터페이스를 사용하여 상대방의 타입을 일일이 검사하지않고 총에 공격을 당한 상대방이
+    //이 인터페이스를 가지고있는지만 감지하여 아래 메소드를 실행시킨다
+
+    //공격을 성공했는지 실패했는지 반환해야한다
     bool ApplyDamage(DamageMessage damageMessage);
+    //DamageMessage는 공격을 한 측에서 공격을 당하는 측에게 전달하는 정보가 포함된 구조체
 }
