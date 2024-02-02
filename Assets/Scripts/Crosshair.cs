92b09f1a0a335dddddba845b761b5a166ccffd58
Rayelle <equation0703@naver.com>
2024-02-02 22:39:06 +0900

    UI

--- Assets/Scripts/Crosshair.cs
+++ Assets/Scripts/Crosshair.cs
@@ -3,33 +3,37 @@ using UnityEngine.UI;
 
 public class Crosshair : MonoBehaviour
 {
-    public Image aimPointReticle;
-    public Image hitPointReticle;
+    public Image aimPointReticle;//조준하는 위치
+    public Image hitPointReticle;//실제로 맞게되는 위치
 
-    public float smoothTime = 10f;
+    public float smoothTime = 0.2f;//지연시간
     
-    private Camera screenCamera;
+    private Camera screenCamera;//씬의 메인카메라
     private RectTransform crossHairRectTransform;
 
-    private Vector3 currentHitPointVelocity;
-    private Vector2 targetPoint;
+    private Vector2 currentHitPointVelocity;//Smooth에 사용할 값의 변화량
+    private Vector2 targetPoint;//UpdatePosition에 들어온 월드포인트를 
+    //스크린 포지션 화면상의 위치로 변환하는 위치
 
-    private void Awake()
-    {
-
+    private void Awake(){
+        screenCamera = Camera.main;
+        crossHairRectTransform = hitPointReticle.GetComponent<RectTransform>();
     }
 
-    public void SetActiveCrosshair(bool active)
-    {
-
+    public void SetActiveCrosshair(bool active){
+        hitPointReticle.enabled = active;
+        aimPointReticle.enabled = active;
     }
 
-    public void UpdatePosition(Vector3 worldPoint)
-    {
+    public void UpdatePosition(Vector3 worldPoint){
+        targetPoint = screenCamera.WorldToScreenPoint(worldPoint);
+        //world point를 screen point로 변경해서 vector2값으로 넣어준다
     }
 
-    private void Update()
-    {
+    private void Update(){
+        if(!hitPointReticle.enabled) return;
+        crossHairRectTransform.position = Vector2.SmoothDamp(crossHairRectTransform.position,
+        targetPoint, ref currentHitPointVelocity, smoothTime); 
 
     }
 }
--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -4,18 +4,19 @@ using UnityEngine.UI;
 
 public class UIManager : MonoBehaviour
 {
+    //씬에 존재하는 UI매니저 타입의 오브젝트를 다른 스크립트에서 접근하도록 싱글톤으로 구현
     private static UIManager instance;
     
-    public static UIManager Instance
-    {
-        get
-        {
+    public static UIManager Instance{
+        get{
             if (instance == null) instance = FindObjectOfType<UIManager>();
 
             return instance;
         }
     }
 
+    //각각의 UI요소들을 외부에서 접근할 필요가 없기에 private으로 선언
+    //인스펙터창에서는 UI들을 할당할수있도록 SerialzieField 필드속성 사용
     [SerializeField] private GameObject gameoverUI;
     [SerializeField] private Crosshair crosshair;
 
@@ -27,46 +28,55 @@ public class UIManager : MonoBehaviour
 
     public void UpdateAmmoText(int magAmmo, int remainAmmo)
     {
+        //남은 탄약수 갱신
         ammoText.text = magAmmo + "/" + remainAmmo;
     }
 
     public void UpdateScoreText(int newScore)
     {
+        //점수 표시
         scoreText.text = "Score : " + newScore;
     }
     
     public void UpdateWaveText(int waves, int count)
     {
+        //남은 웨이브
         waveText.text = "Wave : " + waves + "\nEnemy Left : " + count;
     }
 
     public void UpdateLifeText(int count)
     {
+        //남은 목숨수
         lifeText.text = "Life : " + count;
     }
 
     public void UpdateCrossHairPosition(Vector3 worldPosition)
     {
+        //조준점을 worldPosition위치를 표시하는 위치로 옮겨준다 
         crosshair.UpdatePosition(worldPosition);
     }
     
     public void UpdateHealthText(float health)
     {
+        //체력표시
         healthText.text = Mathf.Floor(health).ToString();
     }
     
     public void SetActiveCrosshair(bool active)
     {
+        //조준점을 표지할지 말지 결정
         crosshair.SetActiveCrosshair(active);
     }
     
     public void SetActiveGameoverUI(bool active)
     {
+        //게임오버창 관리
         gameoverUI.SetActive(active);
     }
     
     public void GameRestart()
     {
+        //현재 씬을 다시 로드해서 재시작
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
 }
