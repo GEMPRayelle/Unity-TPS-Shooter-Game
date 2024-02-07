1a01963b9baf7e2619aba447b838f4fe283d8f95
Rayelle <equation0703@naver.com>
2024-02-08 01:40:51 +0900

    Game Manager 코드

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -15,25 +15,26 @@ public class GameManager : MonoBehaviour
     }
 
     private int score;
-    public bool isGameover { get; private set; }
+    public bool isGameover { get; private set; }//자동 프로퍼티
 
     private void Awake()
     {
         if (Instance != this) Destroy(gameObject);
+        //Awake를 실행하는 오브젝트가 싱글톤이 아니면 파괴시킴
     }
     
     public void AddScore(int newScore)
     {
-        if (!isGameover)
+        if (!isGameover)//게임오버가 아니라면
         {
-            score += newScore;
-            UIManager.Instance.UpdateScoreText(score);
+            score += newScore;//점수추가
+            UIManager.Instance.UpdateScoreText(score);//UI갱신
         }
     }
     
     public void EndGame()
     {
-        isGameover = true;
-        UIManager.Instance.SetActiveGameoverUI(true);
+        isGameover = true;//게임오버 true
+        UIManager.Instance.SetActiveGameoverUI(true);//게임오버창 활성화
     }
 }
